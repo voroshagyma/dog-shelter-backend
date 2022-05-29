a694f58a217a31a0715ad1ed1069c28df277b8b4
proeszti <jon.doe.dummystring@gmail.com>
2022-05-29 20:24:43 +0200

    Add validation packages, config create dog dto validations

--- src/dogs/dto/create-dog.dto.ts
+++ src/dogs/dto/create-dog.dto.ts
@@ -1,1 +1,23 @@
-export class CreateDogDto {}
+import { IsString, IsInt, IsDate, IsOptional } from 'class-validator';
+
+export class CreateDogDto {
+
+    @IsOptional()
+    @IsDate()
+    adoptedAt: Date;
+
+    @IsInt()
+    age: number;
+
+    @IsString()
+    breed: string;
+
+    @IsDate()
+    foundAt: Date;
+
+    @IsInt()
+    id: number;
+
+    @IsString()
+    name: string;
+}
