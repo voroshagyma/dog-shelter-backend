7708e9ffbcb94a0b5de395cc9f05a8b9768d0378
proeszti <jon.doe.dummystring@gmail.com>
2022-05-30 20:29:53 +0200

    Remove expiration from jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
+            //ignoreExpiration: false,
             secretOrKey: jwtConstants.secret,
         });
     }
