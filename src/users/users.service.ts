4e410cf0771febea49b00fb318c95fded2bfcb3b
stakkk <jon.doe.dummystring@gmail.com>
2022-05-30 13:09:22 +0200

    Edit user service and module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
+
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'john',
+            password: 'changeme',
+        }
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
