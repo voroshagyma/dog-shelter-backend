97c80387c3b7fe7910ac7f2f50512bf61b9bfbf6
stakkk <jon.doe.dummystring@gmail.com>
2022-05-30 13:08:16 +0200

    Add users module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DogsModule, AuthModule],
+  imports: [DogsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
